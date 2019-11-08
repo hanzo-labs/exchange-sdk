9af2d1b43e08e6b9ea80db54e3e7a3383cb79969
David Tai <david@verus.io>
2019-11-08 16:09:20 -0600

    fix documentation for typedoc

--- src/Order.ts
+++ src/Order.ts
@@ -2,71 +2,71 @@ import uuid from 'uuid'
 import time from './utils/time'
 import Decimal from 'decimal.js'
 
-/*
+/**
  * Type of the order
  */
 export enum OrderType {
-  /*
+  /**
    * Limit order
    */
   LIMIT,
-  /*
+  /**
    * Market order
    */
   MARKET,
 }
 
-/*
+/**
  * Status of the order
  */
 export enum OrderStatus {
-  /*
+  /**
    * Unfilled order (default state)
    */
   UNFILLED,
-  /*
+  /**
    * Partially filled order
    */
   PARTIALLY_FILLED,
-  /*
+  /**
    * Fully filled order
    */
   FILLED,
-  /*
+  /**
    * Partially filled but cancelled order
    */
   PARTIALLY_FILLED_CANCELLED,
-  /*
+  /**
    * Cancelled order
    */
   CANCELLED,
 }
 
-/*
+/**
  * Represents an order that's put in a book
  */
 export default class Order {
-  /*
+  /**
    * Unique order id
    */
   id: string
-  /*
+  /**
    * Type of the order
    */
   type: OrderType
-  /*
+  /**
    * Status of the order
    */
   status: OrderStatus = OrderStatus.UNFILLED
-  /*
+  /**
    * Bid/Ask price
    */
   price: Decimal
-  /*
+  /**
    * Order size
    */
   amount: Decimal
-  /*
+  /**
    * Unix time
    */
   createdAt: number
